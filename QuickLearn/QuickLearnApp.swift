239e88a4c0e9c657eab681f930f924f1fa2ac4e5
TheMoonThatRises <58153205+TheMoonThatRises@users.noreply.github.com>
2023-10-30 15:18:12 -0600

    add swiftlint

--- QuickLearn/QuickLearnApp.swift
+++ QuickLearn/QuickLearnApp.swift
@@ -12,7 +12,7 @@ import SwiftData
 struct QuickLearnApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            LearnSet.self,
+            LearnSet.self
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
